47b9868b38dfd9e92a85fbf483912745801ea386
leo <amanyamy@gmail.com>
2018-05-18 12:04:53 +0300

    added cube

--- demo.py
+++ demo.py
@@ -7,8 +7,9 @@ import pygame
 
 c = Canvas(screen=(700, 700))
 t = Traingle()
+cube = Cube()
 c.add_shape(t)
-
+c.add_shape(cube)
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
